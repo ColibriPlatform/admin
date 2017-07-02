90624c4fb24e92e5756635c4f820c55437855c51
ilhooq <contact@sphilip.com>
2017-07-02 13:05:59 +0200

    Remove the search form in the sidebar

--- views/layouts/sidebar.php
+++ views/layouts/sidebar.php
@@ -46,14 +46,6 @@ $items[] = ['label' => Yii::t('admin', 'System'), 'icon' => 'gear', 'url' => ['#
     <section class="sidebar">
         <?php $this->trigger('colibri.admin.sidebar-top') ?>
 
-        <form class="sidebar-form" method="get" action="#">
-            <div class="input-group">
-              <input type="text" placeholder="Search..." class="form-control" name="q">
-              <span class="input-group-btn">
-                <button class="btn btn-flat" id="search-btn" name="search" type="submit"><i class="fa fa-search"></i></button>
-              </span>
-            </div>
-          </form>
         <?php
         echo SideNav::widget([
             'options' => [
